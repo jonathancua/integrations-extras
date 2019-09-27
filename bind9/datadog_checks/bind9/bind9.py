7a8619e30017e80f114b998f76943ad84db4c06c
Jonathan Cua <jonathan.cua@gmail.com>
2019-09-27 11:44:17 -0700

    make it compatible with 2.2; collect more metrics

--- bind9/datadog_checks/bind9/bind9.py
+++ bind9/datadog_checks/bind9/bind9.py
@@ -31,6 +31,9 @@ class Bind9Check(AgentCheck):
         for counter in self.QUERY_ARRAY:
             self.collectServerMetric(root[0], counter)
 
+        if root.find(".//statistics").attrib.values()[0] == '2.2':
+            self.collectServerMetric2(root)
+
     def getStatsFromUrl(self, dns_url):
         try:
             response = requests.get(dns_url)
@@ -52,10 +55,31 @@ class Bind9Check(AgentCheck):
             self.SendMetricsToAgent(metricName, self.DateTimeToEpoch(name.text))
 
     def collectServerMetric(self, root, queryType):
-        for counter in root.iter("counters"):
-            if counter.get('type') == queryType:
-                for query in counter:
-                    self.SendMetricsToAgent('{}_{}'.format(queryType, query.get('name')), query.text)
+        if root.find(".//statistics").attrib.values()[0] == '2.2':
+            for counter in root.iter(queryType):
+                self.SendMetricsToAgent('{}_{}'.format(queryType, counter.find('name').text), counter.find('counter').text)
+        else:
+            for counter in root.iter("counters"):
+                if counter.get('type') == queryType:
+                    for query in counter:
+                        self.SendMetricsToAgent('{}_{}'.format(queryType, query.get('name')), query.text)
+
+    def collectServerMetric2(self, root):
+        root = root.find('bind/statistics')
+        for counter in root.findall('server/queries-in/rdtype'):
+            self.SendMetricsToAgent('{}_{}'.format('queries_in', counter.find('name').text),
+                counter.find('counter').text
+            )
+
+        for counter in root.find('memory/summary').getchildren():
+            self.SendMetricsToAgent('{}_{}'.format('memory', counter.tag),
+                int(counter.text)
+            )
+
+        for counter in root.findall('views/view/cache/rrset'):
+            self.SendMetricsToAgent('{}_{}'.format('cache_rrset',
+                counter.find('name').text.replace('!', 'NOT_')),
+                counter.find('counter').text)
 
     def SendMetricsToAgent(self, metricName, metricValue):
         self.gauge('bind9.{}'.format(metricName), metricValue)
